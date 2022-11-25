6eb151eb93f2dbad64402011e1d762b7917481f6
aistomin <andrej.istomin.ikeen@gmail.com>
2022-11-25 06:25:54 +0100

    Issue #80. spring-boot is updated to version 3.

--- src/main/java/com/github/aistomin/andys/backend/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/github/aistomin/andys/backend/security/JwtAuthenticationEntryPoint.java
@@ -15,11 +15,11 @@
  */
 package com.github.aistomin.andys.backend.security;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Serial;
 import java.io.Serializable;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
