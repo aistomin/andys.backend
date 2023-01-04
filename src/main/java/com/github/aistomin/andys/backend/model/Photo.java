b737d9e412641430e11a54b1e127a88bbf5bde10
aistomin <andrej.istomin.ikeen@gmail.com>
2023-01-04 04:53:46 +0100

    Issue #76. PhotoController.load() is implemented.

---
+++ src/main/java/com/github/aistomin/andys/backend/model/Photo.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2022-2023, Istomin Andrei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.aistomin.andys.backend.model;
+
+import com.github.aistomin.andys.backend.controllers.photo.PhotoDto;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Data object that stores photo's data.
+ *
+ * @since 0.1
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public final class Photo {
+
+    /**
+     * Length of the photo description.
+     */
+    private static final int DESCRIPTION_LENGTH = 100_000;
+
+    /**
+     * Photo ID.
+     */
+    @Id
+    @GeneratedValue(
+        strategy = GenerationType.IDENTITY
+    )
+    private Long id;
+
+    /**
+     * Photo's description.
+     */
+    @Column(length = Photo.DESCRIPTION_LENGTH)
+    private String description;
+
+    /**
+     * Photo URL.
+     */
+    @Column
+    private String url;
+
+    /**
+     * The date when the photo was created.
+     */
+    @Column
+    private Date createdOn;
+
+    /**
+     * The date when the photo was published.
+     */
+    @Column
+    private Date publishedOn;
+
+    /**
+     * Ctor.
+     *
+     * @param dto Photo DTO.
+     */
+    public Photo(final PhotoDto dto) {
+        this(
+            dto.getId(),
+            dto.getDescription(),
+            dto.getUrl(),
+            dto.getCreatedOn(),
+            dto.getPublishedOn()
+        );
+    }
+}
---
+++ src/main/java/com/github/aistomin/andys/backend/services/impl/PhotoServiceImpl.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022-2023, Istomin Andrei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.aistomin.andys.backend.services.impl;
+
+import com.github.aistomin.andys.backend.controllers.photo.PhotoDto;
+import com.github.aistomin.andys.backend.controllers.photo.Photos;
+import com.github.aistomin.andys.backend.model.PhotoRepository;
+import com.github.aistomin.andys.backend.services.PhotoService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Photo's service's implementation.
+ *
+ * @since 0.1
+ */
+@Service
+public final class PhotoServiceImpl implements PhotoService {
+
+    /**
+     * Photo repository.
+     */
+    private final PhotoRepository repo;
+
+    /**
+     * Ctor.
+     *
+     * @param repository Photo repository.
+     */
+    public PhotoServiceImpl(final PhotoRepository repository) {
+        this.repo = repository;
+    }
+
+    @Override
+    public Photos load() {
+        return new Photos(
+            this.repo.findAll().stream().map(PhotoDto::new).toList()
+        );
+    }
+}
