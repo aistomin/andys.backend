a3b5e698496c4b198e770bcd9c7a56dedf648085
aistomin <andrej.istomin.ikeen@gmail.com>
2023-01-05 08:27:57 +0100

    Issue #103. PhotoController.edit() is implemented.

--- src/main/java/com/github/aistomin/andys/backend/controllers/photo/PhotoController.java
+++ src/main/java/com/github/aistomin/andys/backend/controllers/photo/PhotoController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -71,6 +72,21 @@ public final class PhotoController {
     ) {
         return new ResponseEntity<>(
             this.photos.save(photo), HttpStatus.CREATED
+        );
+    }
+
+    /**
+     * Edit photo.
+     *
+     * @param photo Photo that needs to be updated.
+     * @return Updated photo.
+     */
+    @PutMapping()
+    public ResponseEntity<PhotoDto> edit(
+        @RequestBody final PhotoDto photo
+    ) {
+        return new ResponseEntity<>(
+            this.photos.save(photo), HttpStatus.OK
         );
     }
 
