173ba4866c8544dcd4712facbc69c08047543d4c
aistomin <andrej.istomin.ikeen@gmail.com>
2023-01-09 07:04:01 +0100

    Issue #149. Demo videos are generated.

--- src/main/java/com/github/aistomin/andys/backend/Application.java
+++ src/main/java/com/github/aistomin/andys/backend/Application.java
@@ -16,7 +16,13 @@
 package com.github.aistomin.andys.backend;
 
 import com.github.aistomin.andys.backend.controllers.user.RegistrationDto;
+import com.github.aistomin.andys.backend.controllers.video.VideoDto;
 import com.github.aistomin.andys.backend.services.UserService;
+import com.github.aistomin.andys.backend.services.VideoService;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.Arrays;
+import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +52,12 @@ public class Application {
     private UserService users;
 
     /**
+     * Video service.
+     */
+    @Autowired
+    private VideoService videos;
+
+    /**
      * App entry point.
      *
      * @param args Command line arguments.
@@ -71,13 +83,214 @@ public class Application {
                 .stream()
                 .anyMatch(user -> username.equals(user.getUsername()));
             if (!adminUserExists) {
-                logger.info("Admin user is missing. Let's create it.");
-                final var admin = new RegistrationDto();
-                admin.setUsername(username);
-                admin.setPassword(username);
-                this.users.register(admin);
+                this.createUser(username);
+                this.generateDemoVideos();
             }
             logger.info("Application is up and running.");
         };
+    }
+
+    /**
+     * Create a user with username.
+     *
+     * @param username Username.
+     */
+    private void createUser(final String username) {
+        this.logger.info("Admin user is missing. Let's create it .....");
+        final var admin = new RegistrationDto();
+        admin.setUsername(username);
+        admin.setPassword(username);
+        this.users.register(admin);
+        this.logger.info("Admin user is created.");
+    }
+
+    /**
+     * Generate demo videos.
+     */
+    @SuppressWarnings({
+        "checkstyle:MagicNumber",
+        "checkstyle:LineLength",
+        "checkstyle:MethodLength"
+    })
+    private void generateDemoVideos() {
+        this.logger.info("Generate demo videos .....");
+        this.videos.save(new VideoDto(
+            null,
+            "Matteo Carcassi - Prelude in E Minor // Andrej Istomin",
+            "Matteo Carcassi(1792 – 1853): \"Prelude in E Minor\". Performed by Andrej Istomin.",
+            "https://www.youtube.com/watch?v=FtJ69YrA3Ok",
+            Date.from(
+                LocalDate.of(2017, 8, 20)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Date.from(
+                LocalDate.of(2017, 8, 20)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Arrays.asList(
+                "music", "guitar", "classicalguitar", "classicalmusic", "carcassi"
+            )
+        ));
+        this.videos.save(new VideoDto(
+            null,
+            "Klaus Schindler - Zeit für Träume // Andrej Istomin",
+            "The photos in this video were taken by my wife(checkout her channel, by the way:  https://www.youtube.com/channel/UC810...) in Landkreis Dachau(mostly in Schwabhausen and Altomünster). The lovely place where I live.",
+            "https://www.youtube.com/watch?v=lLe-HB1WLO4",
+            Date.from(
+                LocalDate.of(2017, 8, 20)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Date.from(
+                LocalDate.of(2017, 8, 20)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Arrays.asList(
+                "music", "guitar", "classicalguitar", "germanmusic",
+                "zeitfuertraeume"
+            )
+        ));
+        this.videos.save(new VideoDto(
+            null,
+            "Andrej Istomin - Jenny",
+            "The melody composed and performed by Andrej Istomin except those telephone sounds.",
+            "https://www.youtube.com/watch?v=j_Ve3uiVsaA",
+            Date.from(
+                LocalDate.of(2017, 8, 21)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Date.from(
+                LocalDate.of(2017, 8, 21)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Arrays.asList("music", "guitar", "police", "hobbymusician")
+        ));
+        this.videos.save(new VideoDto(
+            null,
+            "Mauro Giuliani - Sonatina op. 71 no. 1 // Andrej Istomin",
+            "A little bit of an autumn mood :) Music piece is composed by Mauro Giuliani, called \"Sonatina op71 n°1\". The pictures in slide-show are the reproductions of different Russian painters.",
+            "https://www.youtube.com/watch?v=DIEBQxqY1yU",
+            Date.from(
+                LocalDate.of(2017, 11, 11)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Date.from(
+                LocalDate.of(2017, 11, 11)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Arrays.asList("music", "guitar", "classicalguitar", "giuliani")
+        ));
+        this.videos.save(new VideoDto(
+            null,
+            "Blackmore's Night - Minstrel Hall // Andrej Istomin",
+            "The piece is composed by Ritchie Blackmore.",
+            "https://www.youtube.com/watch?v=iTVHH1vMIcI",
+            Date.from(
+                LocalDate.of(2017, 11, 13)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Date.from(
+                LocalDate.of(2017, 11, 13)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Arrays.asList("music", "guitar", "blackmore")
+        ));
+        this.videos.save(new VideoDto(
+            null,
+            "Fernando Sor - Andante // Andrej Istomin",
+            "This time I'm playing \"Andante\" by Fernando Sor. It is the very first time when I try to create a real video instead of slide show which I did for my previous videos. I hope you like it :)",
+            "https://www.youtube.com/watch?v=kbd7a7j0VOE",
+            Date.from(
+                LocalDate.of(2017, 12, 15)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Date.from(
+                LocalDate.of(2017, 12, 15)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Arrays.asList("music", "guitar", "fernandosor", "classicalguitar")
+        ));
+        this.videos.save(new VideoDto(
+            null,
+            "Greensleeves(English traditional) // Andrej Istomin",
+            "Today I'd like to show you my attempt to play instrumental version of the traditional English song \"Greensleeves\".",
+            "https://www.youtube.com/watch?v=DDt7N5KxXrM",
+            Date.from(
+                LocalDate.of(2019, 5, 30)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Date.from(
+                LocalDate.of(2019, 5, 30)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Arrays.asList("music", "guitar", "fernandosor", "classicalguitar")
+        ));
+        this.videos.save(new VideoDto(
+            null,
+            "Ferdinando Carulli - Andantino // Andrej Istomin",
+            "This summer is really hot here in Germany. I decided to create this small summer video where I'm trying to play nice light piece called \"Andantino\" by Ferdinando Carulli (1770-1841).",
+            "https://www.youtube.com/watch?v=Hd05dNHYqAw",
+            Date.from(
+                LocalDate.of(2019, 7, 9)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Date.from(
+                LocalDate.of(2019, 7, 9)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Arrays.asList("music", "guitar", "carulli", "classicalguitar")
+        ));
+        this.videos.save(new VideoDto(
+            null,
+            "Johann Pachelbel - Sarabande (Guitar + Recorder) // Andrej Istomin",
+            "This summer is really hot here in Germany. I decided to create this small summer video where I'm trying to play nice light piece called \"Andantino\" by Ferdinando Carulli (1770-1841).",
+            "https://www.youtube.com/watch?v=hRvXB_gnR84",
+            Date.from(
+                LocalDate.of(2020, 6, 13)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Date.from(
+                LocalDate.of(2020, 6, 13)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Arrays.asList(
+                "music", "guitar", "pachelbel", "classicalguitar", "recorder"
+            )
+        ));
+        this.videos.save(new VideoDto(
+            null,
+            "J. S. Bach - Bourrée // Andrej Istomin",
+            "Thanks for watching this video. If you like it, please subscribe to my channel, comment the videos, \"like\" them and share with your friends.",
+            "https://www.youtube.com/watch?v=AjQBAQ1jzwc",
+            Date.from(
+                LocalDate.of(2021, 4, 9)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Date.from(
+                LocalDate.of(2021, 4, 9)
+                    .atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+            ),
+            Arrays.asList("music", "guitar", "bach", "classicalguitar")
+        ));
+        this.logger.info("Demo videos were successfully generated.");
     }
 }
