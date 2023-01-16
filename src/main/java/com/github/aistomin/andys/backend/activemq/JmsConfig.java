6c648ace998ff0ba582f9b585152e4fe1e322af9
aistomin <andrej.istomin.ikeen@gmail.com>
2023-01-16 18:17:29 +0100

    Issue #123. JMS and email's logic stub is implemented.

---
+++ src/main/java/com/github/aistomin/andys/backend/activemq/JmsConfig.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2022-2023, Istomin Andrei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.aistomin.andys.backend.activemq;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+
+/**
+ * JMS configuration.
+ *
+ * @since 0.2
+ */
+@Configuration
+@EnableJms
+public class JmsConfig {
+
+    /**
+     * Broker URL.
+     */
+    @SuppressWarnings("checkstyle:LineLength")
+    private static final String BROKER_URL = "failover:tcp://localhost:61616?wireFormat.maxInactivityDurationInitalDelay=30000";
+
+    /**
+     * AMQ username.
+     */
+    private static final String BROKER_USERNAME = "admin";
+
+    /**
+     * AMQ password.
+     */
+    private static final String BROKER_PASSWORD = "admin";
+
+    /**
+     * Create connection factory.
+     *
+     * @return Connection factory.
+     */
+    @Bean
+    public ActiveMQConnectionFactory connectionFactory() {
+        final var factory = new ActiveMQConnectionFactory();
+        factory.setBrokerURL(BROKER_URL);
+        factory.setPassword(BROKER_USERNAME);
+        factory.setUserName(BROKER_PASSWORD);
+        return factory;
+    }
+
+    /**
+     * Create JMS template.
+     *
+     * @return JMS template.
+     */
+    @Bean
+    public JmsTemplate jmsTemplate() {
+        final var template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactory());
+        return template;
+    }
+
+    /**
+     * Create JMS listener container factory.
+     *
+     * @return JMS listener container factory.
+     */
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        final var factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory());
+        return factory;
+    }
+}
---
+++ src/test/java/com/github/aistomin/andys/backend/activemq/EmailProcessorTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2022-2023, Istomin Andrei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.aistomin.andys.backend.activemq;
+
+import com.github.aistomin.andys.backend.model.EmailMessage;
+import com.github.aistomin.andys.backend.model.EmailMessageRepository;
+import com.github.aistomin.andys.backend.model.EmailMessageStatus;
+import com.github.aistomin.andys.backend.model.Person;
+import com.github.aistomin.andys.backend.model.PersonRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Test for {@link EmailProcessor} and {@link EmailSender}.
+ *
+ * @since 0.2
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+final class EmailProcessorTest {
+
+
+    /**
+     * Person repository.
+     */
+    @Autowired
+    private PersonRepository persons;
+
+    /**
+     * Email message repository.
+     */
+    @Autowired
+    private EmailMessageRepository emails;
+
+    /**
+     * Email sender.
+     */
+    @Autowired
+    private EmailSender sender;
+
+    /**
+     * Check that we are correctly sending emails.
+     *
+     * @throws InterruptedException If something goes wrong.
+     */
+    @Test
+    void testProcessEmail() throws InterruptedException {
+        final Person john = this.persons.save(
+            new Person(
+                null,
+                "John",
+                "Doe",
+                "test@successful.email"
+            )
+        );
+        final Person max = this.persons.save(
+            new Person(
+                null,
+                "Max",
+                "Mustermann",
+                "test@failed.email"
+            )
+        );
+        final EmailMessage failed = this.sender.sendEmail(
+            john, max, "Failed email", "Hello Max!"
+        );
+        Thread.sleep(3_000);
+        Assertions.assertEquals(
+            EmailMessageStatus.FAILED,
+            emails.findById(failed.getId()).get().getStatus()
+        );
+        final EmailMessage successful = this.sender.sendEmail(
+            max, john, "Successful email",
+            "Hello John! Haven't heard from you a long time!"
+        );
+        Thread.sleep(3_000);
+        Assertions.assertEquals(
+            EmailMessageStatus.SENT,
+            emails.findById(successful.getId()).get().getStatus()
+        );
+    }
+}
